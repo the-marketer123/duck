96739968d940f0cab7363463db35b587573f8b5d
the-marketer123 <gm11602@icloud.com>
2025-05-08 21:35:48 -0400

    ponds

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ import models from './models.js'
 let map = function(scene,world) {
     models.createGround(scene,world)
     let ponds = []
-    ponds.push(models.createPond());
+    ponds.push(models.createPond(new THREE.Vector3(0,2,0),new THREE.Quaternion(0, 0, 0, 1),30,30));
     ponds.forEach(p=>{console.log(p);scene.add(p)})
     function material (color){
         return new THREE.MeshStandardMaterial({color:color,side: THREE.DoubleSide})
